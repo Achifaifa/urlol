d4ee8585d2626e08c0fe35ccebcce67f2eba9d98
Yuri Numerov <achi@hush.ai>
2016-03-16 15:02:18 +0100

    added scroller

--- urlol.js
+++ urlol.js
@@ -1,6 +1,6 @@
+//THX to http://glench.com/hash/ for the idea and the better way to do this!
 step=0
 beat=0
-
 starttime=Date.now()
 
 function updateclock(){
@@ -10,12 +10,121 @@ function updateclock(){
   beat=Math.floor(deltat/344.8276)
 }
 
+function repeat(pattern, count) {
+  if (count < 1) return '';
+  var result = '';
+  while (count > 1) {
+    if (count & 1) result += pattern;
+    count >>= 1, pattern += pattern;
+  }
+  return result + pattern;
+}
+
+function loading(){
+
+  ;
+}
+
+function startrail(){
+
+  ;
+}
+
+function greets(){
+
+  ;
+}
+
+function fire(){
+
+  ;
+}
+
+function pong(){
+
+  ;
+}
+
+function snake(){
+
+  ;
+}
+
+function scroll(){
+
+  ;
+}
+
+function sinescroll(text){
+
+  len=40-(Math.floor(step/3))
+  string=""
+  for (i=0; i<40; i++){
+    if (i<len){string=string+" "}
+    else {string=string+text[i-len]}
+  }
+  string=string.replace(/undefined/g, '')
+  return string
+}
+
+function invaders(){
+
+  ;
+}
+
+function dunnolol(){
+
+  ;
+}
+
+function gelogo(){
+
+  ;
+}
+
+function kitt(){
+
+  ;
+}
+
+function danceparty(){
+
+  ;
+}
+
+function equalizer(){
+
+  ;
+}
+
+function train(){
+
+  ;
+}
+
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+
+//window.location.replace('#'+str)
+//document.title=str
+
+test=1
+
 function main(){
 
   updateclock()
 
-  str=["WOO","HOO"][beat%2]
-  window.location.replace('#'+str)
+  //// TEST ZONE
+  str=sinescroll("TEST")
+
+  //// SUPER SERIOUS ZONE
+  if (test==0){
+
+    ;
+  }
+
+  console.log(str)
+  window.location.replace('# '+str)
 }
 
 function start(){
